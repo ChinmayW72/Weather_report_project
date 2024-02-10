0f68bb3c1f77d3a944d21bcfa9a334339834c8c3
ChinmayW72 <159627244+ChinmayW72@users.noreply.github.com>
2024-02-10 19:03:01 +0530

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dc890931e3msh1f9a6aec11a1036p103799jsn7586b4edb780',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
